dcbe2824828b5f9ec01c88385b5262a47a3b4d38
Guillman07 <guillem.ma.dev@gmail.com>
2024-10-21 17:47:25 +0200

    Credits Scene 0.2

--- Source/CreditsScene.cpp
+++ Source/CreditsScene.cpp
@@ -1,26 +1,64 @@
 #include "CreditsScene.h"
 
-//void CreditsScene::Update()
-//{
-//	delay.Start();
-//
-//	int opacity = 255;
-//
-//	if (delay.ReadSec() < 3)
-//	{
-//		DrawRectangle(0, 0, Window_Height, Window_width, WHITE);
-//	}
-//	else if (delay.ReadSec() < 6) {
-//
-//		DrawTexture(texture_credits, 0, 0, WHITE);
-//	}
-//	else if(delay.ReadSec() < 9){
-//		
-//		DrawTexture(texture_credits, 0, 0, {255,255,255,opacity});
-//		opacity--;
-//	}
-//	else {
-//		//Game
-//	}
-//	
-//}
+CreditsScene::CreditsScene(Application* app, bool start_enabled) : ModuleGame(app, start_enabled)
+{
+
+}
+
+CreditsScene::~CreditsScene()
+{}
+
+bool CreditsScene::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	switch (language)
+	{
+	case CreditsScene::language::NA:
+		texture_credits = texture_credits_NA;
+		break;
+	case CreditsScene::language::EU:
+		texture_credits = texture_credits_EU;
+		break;
+	case CreditsScene::language::JAPAN:
+		texture_credits = texture_credits_JAPAN;
+		break;
+	default:
+		break;
+	}
+
+	delay.Start();
+	float opacity = 255;
+
+	return ret;
+}
+
+update_status CreditsScene::Update()
+{
+	if (delay.ReadSec() < 3)
+	{
+		DrawRectangle(0, 0, SCREEN_HEIGHT, SCREEN_WIDTH, WHITE);
+	}
+	else if (delay.ReadSec() < 6) {
+
+		DrawTexture(texture_credits, 0, 0, WHITE);
+	}
+	else if(delay.ReadSec() < 9){
+		
+		DrawTexture(texture_credits, 0, 0, {255,255,255,opacity});
+		opacity--;
+	}
+	else {
+		//Game
+	}
+	
+	return UPDATE_CONTINUE;
+}
+
+bool CreditsScene::CleanUp()
+{
+	LOG("Unloading Credits scene");
+
+	return true;
+}
--- Source/CreditsScene.h
+++ Source/CreditsScene.h
@@ -1,27 +1,35 @@
+#include "Globals.h"
 #include "raylib.h"
 #include "Timer.h"
+#include "ModuleGame.h"
 
-//class CreditsScene{
-//public:
-//
-//	void PreUpdate();
-//	void Update();
-//	void PostUpdate();
-//	void Render();
-//
-//	bool CleanUp();
-//
-//private:
-//
-//	Timer delay;
-//
-//	float opacity;
-//
-//	Texture2D* texture_credits;
-//
-//	enum class language {
-//		NA,
-//		EU,
-//		JAPAN
-//	};
-//};
+class CreditsScene : public ModuleGame {
+private:
+
+	Timer delay;
+
+	float opacity;
+
+	Texture2D* texture_credits_EU;
+	Texture2D* texture_credits_NA;
+	Texture2D* texture_credits_JAPAN;
+
+	Texture2D* texture_credits;
+
+	enum class language {
+		NA,
+		EU,
+		JAPAN
+	};
+public:
+
+	CreditsScene(Application* app, bool start_enabled = true);
+	~CreditsScene();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+	language language;
+
+};
