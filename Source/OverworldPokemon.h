870cb8de462c8c6b7c50f20870bf522c2697dde9
Ana Alcázar Cobo <ana.alcazar@estudiantat.upc.edu>
2024-11-15 14:57:57 +0100

    Update

---
+++ Source/OverworldPokemon.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ScreenProgram.h"
+class OverworldPokemon : public ScreenProgram
+{
+private:
+	Texture* overworldPokemon;
+	Texture*
+public:
+	OverworldPokemon();
+	~OverworldPokemon();
+
+};
