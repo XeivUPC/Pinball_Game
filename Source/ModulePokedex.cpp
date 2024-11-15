7536f24d65f626d0ef953a38848011ad97aef4df
Xeiv360 <adria.garcia.almaden@estudiantat.upc.edu>
2024-11-15 02:52:32 +0100

    PokedexCompleted xml

--- Source/ModulePokedex.cpp
+++ Source/ModulePokedex.cpp
@@ -425,20 +425,20 @@ void ModulePokedex::LoadPokedex()
             
         pokemon_list.push_back(pokemon);
     }
-    for (size_t i = 0; i < 18; i++)
+    /*for (size_t i = 0; i < 18; i++)
     {
         printf("%d ->", i);
         for (size_t x = 0; x < pokemon_list.size(); x++)
         {
-            for (size_t y = 0; y < pokemon_list[x].redMapLocations.size(); y++)
+            for (size_t y = 0; y < pokemon_list[x].blueMapLocations.size(); y++)
             {
-                if(pokemon_list[x].redMapLocations[y] == i)
+                if(pokemon_list[x].blueMapLocations[y] == i)
                     printf(" %s,", pokemon_list[x].Names[1].c_str());
                     
             }
         }
         printf("\n");
-    }
+    }*/
 }
 
 void ModulePokedex::DiscoverPokemon(int id)
